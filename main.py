61c489a729c518236ec434e220233791afe7f889
Simantha Hong <simorah@gmail.com>
2014-03-18 01:30:15 +0000

    good stuff

---
+++ main.py
@@ -0,0 +1,37 @@
+""" main.py is the top level script.
+
+Return "Hello World" at the root URL.
+"""
+
+import os
+import sys
+
+# sys.path includes 'server/lib' due to appengine_config.py
+from flask import Flask
+from flask import render_template
+app = Flask(__name__.split('.')[0])
+
+
+#@app.route('/')
+#@app.route('/<name>')
+#def hello(name=None):
+ # """ Return hello template at application root URL."""
+ # return render_template('hello.html', name=name)
+
+@app.route('/')
+def me(name=None):
+  """ Return me template at application /me URL."""
+  return render_template('catwalk.html', name=name)
+
+@app.route('/catwalk')
+def catwalk():
+  return render_template('catwalk.html')
+
+@app.route('/madlibs')
+def madlibs():
+  return render_template('madlibs.html')    
+
+
+
+
+
